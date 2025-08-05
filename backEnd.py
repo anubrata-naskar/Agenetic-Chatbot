f0eabf16eb27331f65d3948b589d6343576d4c3b
Anubrata Naskar <anubrata.naskar@gmail.com>
2025-08-05 07:23:37 +0530

    chatbot simple ui ready for interaction

---
+++ backEnd.py
@@ -0,0 +1,39 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.memory import MemorySaver
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langgraph.graph.message import add_messages
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+    
+    
+chat_model = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    temperature=0.7,
+    max_tokens=1000
+)
+
+def chat_node(state: ChatState) -> ChatState:
+    messages = state['messages'] if state['messages'] else [HumanMessage(content="Hello!")]
+
+    ai_response = chat_model.invoke(messages)
+
+    return {
+        'messages': messages + [AIMessage(content=ai_response.content)]
+    }    
+    
+checkpointer = MemorySaver()
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)    
---
+++ frontEnd.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage, AIMessage
+from backEnd import chatbot
+
+thread_id='1'
+config = {'configurable': {'thread_id': thread_id}}
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+for message in st.session_state['message_history']:
+    if message['role'] == 'user':
+        with st.chat_message('user'):
+            st.write(message['content'])
+    else:
+        with st.chat_message('assistant'):
+            st.write(message['content'])
+    
+user_input = st.chat_input('type here ')    
+
+if user_input:
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.write(user_input)
+        
+    chatbot_response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=config)   
+    ai_message = chatbot_response['messages'][-1] 
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message.content})
+    with st.chat_message('assistant'):
+        st.write(ai_message.content)
+        
+
+        
