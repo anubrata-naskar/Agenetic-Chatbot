b56afd4cdf55bba6f95e5e29181219f4eaca4c2d
Anubrata Naskar <anubrata.naskar@gmail.com>
2025-08-09 09:36:18 +0530

    itegrate sqlite databse

---
+++ backEndDB.py
@@ -0,0 +1,51 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langgraph.graph.message import add_messages
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+    
+    
+chat_model = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    temperature=0.7,
+    max_tokens=1000
+)
+
+def chat_node(state: ChatState) -> ChatState:
+    messages = state['messages']
+
+    ai_response = chat_model.invoke(messages)
+
+    return {
+        'messages': messages + [AIMessage(content=ai_response.content)]
+    }    
+
+
+conn = sqlite3.connect('chatbot.db', check_same_thread=False)
+    
+checkpointer = SqliteSaver(conn)
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)  
+
+
+def get_all_threads():
+    all_threads = set()
+    for c in checkpointer.list(None):
+        all_threads.add(c.config['configurable']['thread_id'])  
+    return list(all_threads) 
